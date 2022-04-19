d0595338b24a619c1b5fed3a9ea5e87b55f7f4df
unknown <ychen@ucsd.edu>
2022-04-18 19:19:46 -0700

    Q1

---
+++ src/TextMessage.java
@@ -0,0 +1,24 @@
+public class TextMessage extends Message {
+
+    /* Error message to use in OperationDeniedException */
+    private static final String EXCEED_MAX_LENGTH =
+            "Your input exceeded the maximum length limit.";
+
+    public TextMessage(User sender, String text)
+            throws OperationDeniedException {
+        if (sender == null || text = null) {
+            throw new IllegalArgumentException();
+        }
+        if (text.length() > 500) {
+            throw new OperationDeniedException(EXCEED_MAX_LENGTH);
+        }
+        super(sender;
+        this.contents = text;
+    }
+
+    public String getContents() {
+        /* TODO */
+        return null;
+    }
+
+}
